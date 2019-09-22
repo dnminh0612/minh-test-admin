5fa4e1a505d7d9ef5c54e3d8e36d6e1b4b5aad50
NHAT MINH <vitop.minhdoan@gmail.com>
2019-09-22 11:50:35 +0700

    123

--- config/app.php
+++ config/app.php
@@ -165,7 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Laravel\Tinker\TinkerServiceProvider::class,
+        TCG\Voyager\VoyagerServiceProvider::class,
         /*
          * Application Service Providers...
          */
